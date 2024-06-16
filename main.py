185316ca7de73abe6101eba04721884311852066
prince-ao <prince.addo3.141@gmail.com>
2024-06-16 07:15:22 -0400

    first route

---
+++ main.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+import random
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    path = event['rawPath']
+    request_method = event['requestContext']['http']['method']
+    bucket_name = 'anderson-api'
+    response = 'unable to process your request'
+
+    if request_method == 'GET':
+        if path == '/api/jokes/random':
+            file_key = 'jokes.json'
+            jokes = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+            parsed_jokes = json.load(response['Body'])
+
+            current_time = int(time.time())
+            random.seed(current_time)
+
+            random_index = random.randint(0, len(parsed_jokes) - 1)
+
+            return {
+                'statusCode': 404,
+                'body': json.dumps(parsed_jokes[random_index])
+            }
+
+
+
+    return {
+        'statusCode': 404,
+        'body': json.dumps(f'404: request {request_method} {path} not found')
+    }
+
